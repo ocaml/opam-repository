d65a2811cac05820e3d7608adb8e77955acec081
Anil Madhavapeddy <anil@recoil.org>
2015-03-20 18:06:32 +0000

    Fix up the Travis CI setup for OPAM 1.2.1 release
    - remove obsolete OPAM 1.0.0 test
    - reduce the number of OPAM 1.1.1 tests (just OCaml 4.02.1 and OCaml 4.01.1)
    - add a new OPAM 1.2.0-specific PPA to ensure we test this version as well as 1.2.1
    
    Note that the OSX tests still need an associated bump after the
    merge of Homebrew/homebrew#37906

--- .travis-ci-install.sh
+++ .travis-ci-install.sh
@@ -8,18 +8,15 @@ full_apt_version () {
 install_on_linux () {
   # Install OCaml and OPAM PPAs
   case "$OCAML_VERSION,$OPAM_VERSION" in
-  3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-  3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+  3.12.1,1.1.1) ppa=avsm/ocaml312+opam11 ;;
   3.12.1,1.2.0) ppa=avsm/ocaml312+opam12 ;;
-  4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-  4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-  4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
-  4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-  4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-  4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
-  4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
-  4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
-  4.02.1,1.2.1) ppa=avsm/ppa-opam-experimental ;;
+  4.00.1,1.1.1) ppa=avsm/ocaml40+opam11 ;;
+  4.00.1,1.2.1) ppa=avsm/ocaml40+opam12 ;;
+  4.01.0,1.1.1) ppa=avsm/ocaml41+opam11 ;;
+  4.01.0,1.2.1) ppa=avsm/ocaml41+opam12 ;;
+  4.02.1,1.1.1) ppa=avsm/ocaml42+opam11 ;;
+  4.02.1,1.2.0) ppa=avsm/ocaml42+opam120 ;;
+  4.02.1,1.2.1) ppa=avsm/ocaml42+opam12 ;;
   *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
   esac
 
