cde460af9078afe99eb4005705a91eeb0a91d6dc
Hannes Mehnert <hannes@mehnert.org>
2015-05-14 16:31:20 +0100

    release gmp-xen, a gmp crosscompiled for xen (from mirage-dev) adjust zarith to be crosscompiled for xen as well

---
+++ packages/gmp-xen/gmp-xen.6.0.0/files/mirage-build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -ex
+PREFIX=`opam config var prefix`
+PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH
+
+CPPFLAGS="$CPPFLAGS `pkg-config mirage-xen-posix --cflags` -O2 -pedantic -fomit-frame-pointer -fno-builtin -D_FORTIFY_SOURCE=0 -Wmissing-prototypes --std=gnu99"
+# Use different --host and --build to trigger cross-compilation mode (don't try to test binaries during configure)
+# Set CC to stop it trying to find a separate compiler for HOST.
+# Pass CPPFLAGS (not just CFLAGS) to stop it finding the Linux headers (just generates warnings).
+# Use -Werror=missing-prototypes because we're not running the tests due to cross-compiling.
+HOST="`uname -m`-unknown-none"
+BUILD=`./config.guess`
+./configure --host="$HOST" --build="$BUILD" --enable-fat CC=gcc --prefix="$PREFIX/lib/gmp-xen" --disable-shared CPPFLAGS="$CPPFLAGS"
+# Because we're cross-compiling, configurate can't tell whether a function
+# actually exists and just assumes they all do. For localeconv, this is wrong.
+sed -e '/HAVE_LOCALECONV/d'  \
+    -e '/HAVE_OBSTACK_VPRINTF/d'\
+    -i config.status
+./config.status
+make
