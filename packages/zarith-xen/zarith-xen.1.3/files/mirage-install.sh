45af14630e4dd63d98d8486c7d43c6a93a75d3e1
Hannes Mehnert <hannes@mehnert.org>
2015-05-20 13:45:58 +0100

    revert zarith patches, introduce zarith-xen, adjust nocrypto

--- packages/zarith-xen/zarith-xen.1.3/files/mirage-install.sh
+++ packages/zarith-xen/zarith-xen.1.3/files/mirage-install.sh
@@ -8,7 +8,6 @@ if pkg-config --exists mirage-xen; then
   LDFLAGS=`pkg-config --libs gmp-xen`
   export LDFLAGS
 
-  make clean
   # WARNING: if you pass invalid cflags here, zarith will silently
   # fall back to compiling with the default flags instead!
   CFLAGS="`pkg-config --cflags gmp-xen mirage-xen` -O2 -pedantic -fomit-frame-pointer -fno-builtin"
@@ -16,4 +15,5 @@ if pkg-config --exists mirage-xen; then
   ./configure
   make
   cp libzarith.a "$PREFIX/lib/zarith/libzarith-xen.a"
+  cp META "$PREFIX/lib/zarith/META"
 fi
