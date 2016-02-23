4c99b2b1394cf335f9d5812656363aa143d24bdc
Grégoire Henry <gregoire.henry@ocamlpro.com>
2016-02-23 21:02:41 +0100

    Hide OCaml build's log with opam-1.1

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -60,6 +60,7 @@ function opam_version_compat {
   OPAM_MINOR=${OPAM_VERSION#*.}
   OPAM_MINOR=${OPAM_MINOR%%.*}
   if [ $OPAM_MAJOR -eq 1 ] && [ $OPAM_MINOR -lt 2 ]; then
+      opam_version_11=1
       ocamlv=$(ocamlrun -vnum)
       bytev=${ocamlv%.*}
       curl -L https://opam.ocaml.org/repo_compat_1_1.byte$bytev -o compat.byte
@@ -72,7 +73,18 @@ function build_one {
   pkg=$1
   echo "build one: $pkg ($OPAM_SWITCH)"
   rm -rf ~/.opam
-  opam init . --comp=$OPAM_SWITCH
+  if [ -n "${opam_version_11}" ]; then
+      # Hide OCaml build log
+      if opam init . --comp=$OPAM_SWITCH > build.log 2>&1 ; then
+          echo -n
+      else
+          rc=$?
+          cat build.log
+          exit $rc
+      fi
+  else
+      opam init . --comp=$OPAM_SWITCH
+  fi
   eval `opam config env`
   # test for installability
   echo "Checking for availability..."
