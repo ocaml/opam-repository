a4b4240fa716ee70a6c03a7783aea6f9253a8e05
Hannes Mehnert <hannes@mehnert.org>
2016-07-06 15:20:54 +0100

    now that we're on trusty, no need anymore for this hack!

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -104,20 +104,6 @@ function build_one {
       fi
   else
     echo "... package available."
-    case $TRAVIS_OS_NAME in
-        linux)
-            # we need fresh gcc and binutils, maybe...
-            # this can soon be removed, once travis upgraded their infrastructure
-            if [ `opam install --dry-run $pkg | grep -c mirage-entropy-xen` -gt 0 ] ; then
-                echo "installing a recent gcc and binutils (mainly to get mirage-entropy-xen working\!)"
-                sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-                sudo apt-get -qq update
-                sudo apt-get install -y gcc-4.8
-                sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
-                wget http://mirrors.kernel.org/ubuntu/pool/main/b/binutils/binutils_2.24-5ubuntu3.1_amd64.deb
-                sudo dpkg -i binutils_2.24-5ubuntu3.1_amd64.deb
-            fi
-    esac
     echo
     echo "====== External dependency handling ======"
     opam install depext
