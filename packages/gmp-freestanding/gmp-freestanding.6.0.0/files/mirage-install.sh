ad8bb91e82fb7f678157a444ba589d11e6e5cbe6
Tim Cuthbertson <tim@gfxmonk.net>
2017-04-09 17:29:54 +1000

    gmp-freestanding: install (empty) META file

--- packages/gmp-freestanding/gmp-freestanding.6.0.0/files/mirage-install.sh
+++ packages/gmp-freestanding/gmp-freestanding.6.0.0/files/mirage-install.sh
@@ -9,5 +9,6 @@ mkdir -p ${PKG_CONFIG_PATH}
 cp gmp-freestanding.pc ${PKG_CONFIG_PATH}
 mkdir -p ${LIBDIR}
 cp .libs/libgmp.a ${LIBDIR}/libgmp-freestanding.a
+touch ${LIBDIR}/META
 mkdir -p ${LIBDIR}/include
 cp gmp.h ${LIBDIR}/include
