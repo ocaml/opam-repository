5adc18804f1de827fe4efccff921bbed06315f00
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2017-07-17 16:31:29 +0200

    Add conf-llvm.3.8

--- packages/llvm/llvm.3.8/files/install.sh
+++ packages/llvm/llvm.3.8/files/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-version=$1
-make=$2
-prefix=$3
-libdir=$4
+llvm_config="$1"
+make="$2"
+prefix="$3"
+libdir="$4"
 
 common_configure() {
     ../configure CC=gcc CXX=g++ --disable-compiler-version-checks --prefix="$prefix" \
@@ -20,32 +20,17 @@ configure() {
     esac
 }
 
-brew_llvm_config=/usr/local/Cellar/llvm/${version}*/bin/llvm-config
-if ! stat -t $brew_llvm_config; then
-    brew_llvm_config=
-fi
+mkdir build
+cd build
 
+configure
+
+"$make" -C bindings all SYSTEM_LLVM_CONFIG="$llvm_config"
+"$make" -C bindings install SYSTEM_LLVM_CONFIG="$llvm_config"
+
+mv "$libdir/llvm/META.llvm" "$libdir/llvm/META"
 shopt -s nullglob
-
-version_sans_dot=$(echo $version | tr -d .)
-for config in llvm-config-$version llvm-config$version_sans_dot llvm-config-mp-$version $brew_llvm_config llvm-config; do
-    case `$config --version` in
-        $version|$version.*)
-            mkdir build
-            cd build
-            configure
-            $make -C bindings all SYSTEM_LLVM_CONFIG=$config
-            $make -C bindings install SYSTEM_LLVM_CONFIG=$config
-            mv "$libdir/llvm/META.llvm" "$libdir/llvm/META"
-            for stublib in $libdir/llvm/dll*.so; do
-                mv "$stublib" "$libdir/stublibs/"
-                echo llvm >"$libdir/stublibs/$(basename $stublib).owner"
-            done
-            exit 0;;
-        *)
-            continue;;
-    esac
+for stublib in $libdir/llvm/dll*.so; do
+    mv "$stublib" "$libdir/stublibs/"
+    echo llvm > "$libdir/stublibs/$(basename $stublib).owner"
 done
-
-echo "Error: LLVM $version is not installed."
-exit 1
