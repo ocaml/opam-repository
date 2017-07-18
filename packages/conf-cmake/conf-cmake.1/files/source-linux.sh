e90dd1b58b4aa06fd6964350ab724ade081dcb5c
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2017-07-18 20:48:52 +0200

    Remove source predicates from conf-cmake & conf-llvm

--- packages/conf-cmake/conf-cmake.1/files/source-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo add-apt-repository --yes ppa:likemartinma/devel
-sudo apt-get update -qq
-sudo apt-get install -qq cmake
--- packages/conf-llvm/conf-llvm.3.9/files/source-linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.9 main'
-curl -L http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-
-sudo apt-get update -qq
-
-sudo apt-get install -qq llvm-3.9-dev
