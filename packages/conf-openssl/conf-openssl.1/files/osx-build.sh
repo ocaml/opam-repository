a54ef5f1241897f7a393fa57de901e34fd045e90
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-12-13 12:30:52 +0000

    Add correct command now

--- packages/conf-openssl/conf-openssl.1/files/osx-build.sh
+++ packages/conf-openssl/conf-openssl.1/files/osx-build.sh
@@ -2,7 +2,7 @@
 
 if [ -d "$HOME/.nix-profile/lib/pkgconfig/" ]; then
   # Nix on macOS
-  PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig openssl
+  PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig pkg-config openssl
 elif [ -d "/usr/local/opt/openssl/" ]; then
   # Homebrew
   PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig pkg-config openssl
