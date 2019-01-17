203540389f2ce7c38b6a743c2fdffc2e4216318d
Corey Richardson <cmr@o1labs.org>
2019-01-17 14:21:29 -0800

    conf-openssl: macos: look for openssl until it is found

--- packages/conf-openssl/conf-openssl.1/files/osx-build.sh
+++ packages/conf-openssl/conf-openssl.1/files/osx-build.sh
@@ -2,11 +2,21 @@
 
 if [ -d "$HOME/.nix-profile/lib/pkgconfig/" ]; then
   # Nix on macOS
-  PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig pkg-config openssl
-elif [ -d "/usr/local/opt/openssl/" ]; then
-  # Homebrew
-  PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig pkg-config openssl
-else
-  # MacPorts
-  PKG_CONFIG_PATH=/opt/local/lib/pkgconfig pkg-config openssl
+  res=$(env PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig pkg-config openssl)
+  if [ $? -eq 0 ]; then
+    echo $res
+    exit 0
+  fi
 fi
+
+if [ -d "/usr/local/opt/openssl/" ]; then
+  # Homebrew
+  res=$(env PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig pkg-config openssl)
+  if [ $? -eq 0 ]; then
+    echo $res
+    exit 0
+  fi
+fi
+
+# MacPorts
+PKG_CONFIG_PATH=/opt/local/lib/pkgconfig pkg-config openssl
