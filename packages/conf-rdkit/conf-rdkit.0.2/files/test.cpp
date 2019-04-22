42cd31f693c4649f43a07d9a5f5dfb347a5cd9c8
Francois Berenger <unixjunkie@sdf.org>
2019-04-22 11:47:12 +0900

    updated post message for conf-rdkit on mac

--- packages/conf-rdkit/conf-rdkit.0.2/files/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include "rdkit/RDGeneral/versions.h"
-
-// compile with: c++ test.cpp -o test -l RDGeneral
-
-int main() {
-  printf("%s\n", RDKit::rdkitVersion);
-  return 0;
-}
