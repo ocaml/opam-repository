aed3750508dc6d320fb6a0b9b677c0da133bd674
Kate <kit.ty.kate@disroot.org>
2021-04-29 20:05:57 +0100

    oranger.2.0.3: Make the install script more portable

--- packages/oranger/oranger.2.0.3/files/compile_ranger.sh
+++ packages/oranger/oranger.2.0.3/files/compile_ranger.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -x
 
@@ -7,6 +7,6 @@ tar xzf 0.9.11.tar.gz
 cd ranger-0.9.11/cpp_version
 mkdir build
 cd build
-cmake ../
-make
+"${2}" ../
+"${3}"
 cp ranger "${1}/ml_rf_ranger"
