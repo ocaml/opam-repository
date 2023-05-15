9018233684396ca2ed8585e176374da5b25d647d
Romain Beauxis <toots@rastageeks.org>
2023-05-15 09:28:28 -0500

    Cleanup

--- .github/scripts/packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-PACKAGES=`git diff --name-only origin/master..HEAD | grep '^packages' | cut -d'/' -f 3 | while read i; do echo "\"$i\""; done | paste -sd ',' -`
-
-echo "Packages to test: $PACKAGES"
-echo "packages=[${PACKAGES}]" >> $GITHUB_OUTPUT
-
