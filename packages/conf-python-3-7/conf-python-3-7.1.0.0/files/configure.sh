e47e9f11cdee429c02158b92a506315eb3893d8e
Nat Karmios <nat@karmios.com>
2023-08-08 03:57:56 +0100

    conf-python-3-7: consider python 3.11

--- packages/conf-python-3-7/conf-python-3-7.1.0.0/files/configure.sh
+++ packages/conf-python-3-7/conf-python-3-7.1.0.0/files/configure.sh
@@ -1,4 +1,4 @@
-for version in 3.10 3.9 3.8 3.7; do
+for version in 3.11 3.10 3.9 3.8 3.7; do
     python_exe="python$version"
     if "$python_exe" test.py; then
         echo "python3: \"$python_exe\"" >> conf-python-3-7.config
